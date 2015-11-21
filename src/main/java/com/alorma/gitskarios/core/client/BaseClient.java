5a4e3e5780e262d9e25cbb777f91cb015568476b
Bernat Borrás Paronella <bernat.borras@scmspain.com>
2015-11-21 21:06:20 +0100

    add retry

--- src/main/java/com/alorma/gitskarios/core/client/BaseClient.java
+++ src/main/java/com/alorma/gitskarios/core/client/BaseClient.java
@@ -3,11 +3,14 @@ package com.alorma.gitskarios.core.client;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import com.alorma.gitskarios.core.ApiClient;
+import java.util.concurrent.TimeUnit;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
 import retrofit.client.Client;
 import retrofit.converter.Converter;
 import rx.Observable;
+import rx.functions.Func2;
 
 public abstract class BaseClient<K> implements RequestInterceptor, RestAdapter.Log {
 
@@ -47,7 +50,15 @@ public abstract class BaseClient<K> implements RequestInterceptor, RestAdapter.L
   }
 
   public Observable<K> observable() {
-    return getApiObservable(getRestAdapter());
+    return getApiObservable(getRestAdapter()).retry(new Func2<Integer, Throwable, Boolean>() {
+      @Override
+      public Boolean call(Integer integer, Throwable throwable) {
+        if (throwable instanceof RetrofitError) {
+          return ((RetrofitError) throwable).getResponse().getStatus() == 202;
+        }
+        return integer < 3;
+      }
+    }).debounce(100, TimeUnit.MILLISECONDS);
   }
 
   protected abstract Observable<K> getApiObservable(RestAdapter restAdapter);
